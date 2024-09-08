d0269286b87ede25b95d6d5c2f6fa4e27711bb8c
扑克牌 <162698997+pkp000@users.noreply.github.com>
2024-09-08 16:59:21 +0800

    Add files via upload

---
+++ 密码登录系统/4_1/user_password_interface/user_password_interface/BackSystemDlg.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+extern CString userName0;
+extern CString password0;
+extern CString* p;
+// BackSystemDlg 对话框
+
+class BackSystemDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(BackSystemDlg)
+
+public:
+	BackSystemDlg(CWnd* pParent = nullptr)afx_msg; void OnBnClickedOk();
+	;   // 标准构造函数
+	virtual ~BackSystemDlg();
+	BOOL m_bCanExit;
+
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_Back };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	
+	
+	// 自定义的声明
+private:
+	CString str1;
+	CString str2;
+};
---
+++ 密码登录系统/4_1/user_password_interface/user_password_interface/RegistrationDlg.cpp
@@ -0,0 +1,34 @@
+﻿// RegistrationDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "user_password_interface.h"
+#include "afxdialogex.h"
+#include "RegistrationDlg.h"
+
+
+// RegistrationDlg 对话框
+
+IMPLEMENT_DYNAMIC(RegistrationDlg, CDialogEx)
+
+RegistrationDlg::RegistrationDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+RegistrationDlg::~RegistrationDlg()
+{
+}
+
+void RegistrationDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(RegistrationDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// RegistrationDlg 消息处理程序
